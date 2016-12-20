9036f39a99da50cc8986c6568c1c9e3b56a1cc92
cezbil <czaro93pl@gmail.com>
2016-12-20 02:12:29 +0000

    a

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,19 +3,24 @@
 session_start();
 
 require_once '../vendor/autoload.php';
-$container = new Slim\Container;
 
-$container['flash']= function($c){
-return new \Slim\Flash\Messages;
+$container = new \Slim\Container;
+
+$container['flash'] = function ($c) {
+    return new \Slim\Flash\Messages();
 };
 
-$container['view'] = function ($c){
-  $view = new \Slim\Views\Twig('../resources/views');
-  $view->addExtensions(new \Slim\Views\TwigExtension(
-      $c['router'],
-      $c['request']->getUri()
-  ));
-  return $view;
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig('../resources/views');
+
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $c['router'],
+        $c['request']->getUri()
+    ));
+
+    $view->getEnvironment()->addGlobal('flash', $c['flash']);
+
+    return $view;
 };
 
 $app = new \Slim\App($container);
--- app/routes.php
+++ app/routes.php
@@ -2,4 +2,11 @@
 
 $app->get('/', function ($request, $response, $args) {
     return $this->view->render($response, 'home.twig');
+})->setName('home');
+
+$app->get('/flash', function ($request, $response, $args) use ($app) {
+
+    $this->flash->addMessage('global', 'You have registered!');
+
+    return $response->withRedirect($app->router->pathFor('home'));
 });
